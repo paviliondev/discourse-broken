de46145347d025668ef8b5d369de9af962b82cb2
Angus McLeod <angus@mcleod.org.au>
2020-07-08 10:16:46 +1000

    Add plugin url

--- plugin.rb
+++ plugin.rb
@@ -2,6 +2,7 @@
 # about: An intentionally broken plugin
 # version: 0.2
 # authors: Angus McLeod
+# url: https://github.com/angusmcleod/discourse-broken
 
 after_initialize do
   NotARealClass.new(notarealvariable)
