f4c871330b6d7a14b13cf9a2e0135af7d2ac8d05
Robert Barrow <merefield@gmail.com>
2021-03-10 10:01:31 +0000

    Add multiple emails

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # name: discourse-broken
 # about: An intentionally broken plugin
 # version: 0.3
-# contact emails: robert@thepavilion.io
+# contact emails: robert@thepavilion.io, faizaan@thepavilion.io, angus@thepavilion.io
 # authors: Angus McLeod
 # url: https://github.com/angusmcleod/discourse-broken
 
